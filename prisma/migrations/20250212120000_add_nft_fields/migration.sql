052f1972103122fc0fdc447c497812e2ba5dbcad
wiimdy <wiimdy@wiimdys-MacBook-Pro.local>
2025-11-25 10:53:43 +0900

    Change: db

--- prisma/migrations/20250212120000_add_nft_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- Add NFT-related columns to Attendance
-ALTER TABLE "Attendance" ADD COLUMN "tokenUri" TEXT;
-ALTER TABLE "Attendance" ADD COLUMN "txHash" TEXT;
-ALTER TABLE "Attendance" ADD COLUMN "contractAddress" TEXT;
-ALTER TABLE "Attendance" ADD COLUMN "chainId" TEXT;
